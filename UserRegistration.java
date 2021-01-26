7788a3d037993ef319d4d9498e6e9cba1143ee8c
biswajitdixit <buntyoec1998@gmail.com>
2021-01-26 12:48:48 +0530

    ADDED: regex for first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,31 @@
-public class UserRegistration{
- public static void main(String[]args){
-   System.out.println("Welcome to the user registartion");
- }
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+    public boolean validateFirstName(String FirstName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        boolean res = pattern.matcher(FirstName).matches();
+        return res;
+
+    }
+    public static void main(String[] args) {
+        UserRegistration ur =new UserRegistration ();
+        Scanner bc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("1.First Name Validation");
+        System.out.println("Enter choice");
+
+        int choice =bc.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Enter the first name ");
+                String FirstName= sc.nextLine();
+                if ( ur.validateFirstName(FirstName) ){
+                    System.out.println("valid");
+                }else {
+                    System.out.println("not valid");
+                }
+        }
+    }
 }
