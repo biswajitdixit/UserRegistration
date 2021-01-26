655cad3d00f40ddcfa4e96ea90ce90897e80a09e
biswajitdixit <buntyoec1998@gmail.com>
2021-01-26 13:01:27 +0530

    ADDED: regex for mail

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+    private static final String EMAIL_PATTERN="^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
     public boolean validateFirstName(String FirstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         boolean res = pattern.matcher(FirstName).matches();
@@ -14,12 +15,18 @@ public class UserRegistration {
         boolean res=pattern.matcher(LastName).matches();
         return res;
     }
+    public  boolean validateEmail(String Email){
+        Pattern pattern=Pattern.compile(EMAIL_PATTERN);
+        boolean res=pattern.matcher(Email).matches();
+        return res;
+    }
     public static void main(String[] args) {
         UserRegistration ur =new UserRegistration ();
         Scanner bc=new Scanner(System.in);
         Scanner sc=new Scanner(System.in);
         System.out.println("1.First Name Validation");
         System.out.println("2.Last Name Validation");
+        System.out.println("3.Email Validation");
         System.out.println("Enter choice");
 
         int choice =bc.nextInt();
@@ -37,6 +44,15 @@ public class UserRegistration {
                 System.out.println("Enter the last name");
                 String LastName=sc.nextLine();
                 if (ur.validateLastName(LastName) ){
+                    System.out.println("valid");
+                }else {
+                    System.out.println("not valid");
+                }
+                break;
+            case 3:
+                System.out.println("Enter the email");
+                String Email= sc.nextLine();
+                if (ur.validateEmail(Email)){
                     System.out.println("valid");
                 }else {
                     System.out.println("not valid");
