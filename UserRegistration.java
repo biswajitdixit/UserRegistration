6c738e2803c23c766b27182dc3d5eda3b0dd0476
biswajitdixit <buntyoec1998@gmail.com>
2021-02-03 01:18:06 +0530

    REFACTORED: use lambda function to validate detail

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,52 +6,13 @@ public class UserRegistration {
     private static final String MOBILE_PATTERN = "[91]+[\s]+[0-9]{10}";
     private static final String PASSWORD_PATTERN = "(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\\d]){1,})(?=(.*[\\W]){1,})(?!.*\\s).{8,}$";
 
-    public boolean validateFirstName(String FirstName) throws UserRegistrationException{
-        try {
-            Pattern pattern = Pattern.compile(NAME_PATTERN);
-        boolean res = pattern.matcher(FirstName).matches();
-        return res;}
-        catch (Exception e){
-         throw new UserRegistrationException("invalid entry");
-        }
-
-    }
-
-    public boolean validateLastName(String LastName) throws UserRegistrationException{
-        try {
-            Pattern pattern = Pattern.compile(NAME_PATTERN);
-        boolean res = pattern.matcher(LastName).matches();
-        return res;}catch (Exception e){
-            throw new UserRegistrationException("invalid Entry");
-        }
-    }
-
-    public static boolean validateEmail(String Email) throws UserRegistrationException{
-        try {
-            Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        boolean res = pattern.matcher(Email).matches();
-        return res;}catch (Exception e){
-            throw new UserRegistrationException("invalid Entry");
-        }
-    }
-
-    public boolean validateMobileNo(String MobileNo) throws UserRegistrationException{
-        try {
-            Pattern pattern = Pattern.compile(MOBILE_PATTERN);
-        boolean res = pattern.matcher(MobileNo).matches();
-        return res;}catch (Exception e){
-            throw new UserRegistrationException("invalid Entry");
-        }
-    }
-
-
-
-    public boolean validatePassword(String Password) throws UserRegistrationException{
-       try {
-           Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-        boolean res = pattern.matcher(Password).matches();
-        return res;}catch (Exception e){
-           throw new UserRegistrationException("invalid Entry");
-       }
-    }
+    UserValidator validateFirstName = (s) -> (Pattern.compile(NAME_PATTERN).matcher(s).matches());
+    UserValidator validateLastName = (s) -> (Pattern.compile(NAME_PATTERN).matcher(s).matches());
+    UserValidator validateEmail = (s) -> (Pattern.compile(EMAIL_PATTERN).matcher(s).matches());
+    UserValidator validateMobile = (s) -> (Pattern.compile(MOBILE_PATTERN).matcher(s).matches());
+    UserValidator validatePassword = (s) -> (Pattern.compile(PASSWORD_PATTERN).matcher(s).matches());
+}
+@FunctionalInterface
+interface UserValidator {
+    boolean validate(String s);
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -4,123 +4,48 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     UserRegistration userRegistration = new UserRegistration();
+
     @Test
     public void givenFirstNameValid() {
         boolean result = true;
-        try {
-            result = userRegistration.validateFirstName("Biswa");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
+        result = userRegistration.validateFirstName.validate("Biswa");
         Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenFirstNameInvalid() {
-        boolean result = false;
-        try {
-            result = userRegistration.validateFirstName("bisw");
-        } catch (UserRegistrationException e) {
 
-            e.printStackTrace();
-        }
-        Assert.assertFalse(result);
-    }
 
     @Test
     public void givenLastNameValid() {
         boolean result = true;
-        try {
-            result = userRegistration.validateLastName("Dixit");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
+        result = userRegistration.validateLastName.validate("Dixit");
         Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenLastNameInvalid() {
-        boolean result = false;
-        try {
-            result = userRegistration.validateLastName("Dix");
-        } catch (UserRegistrationException e) {
 
-            e.printStackTrace();
-        }
-        Assert.assertFalse(result);
-    }
 
     @Test
     public void givenEmailValid() {
         boolean result = true;
-        try {
-            result = userRegistration.validateEmail("biswajitdixit7991@gmail.com");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
+        result = userRegistration.validateEmail.validate("biswajitdixit7991@gmail.com");
         Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenEmailInvalid() {
-        boolean result = false;
-        try {
-            result = userRegistration.validateEmail("biswajitdixi");
-        } catch (UserRegistrationException e) {
 
-            e.printStackTrace();
-        }
-        Assert.assertFalse(result);
-    }
 
     @Test
     public void givenMobileNoValid() {
         boolean result = true;
-        try {
-            result = userRegistration.validateMobileNo("91 9437726849");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
+        result = userRegistration.validateMobile.validate("91 9437726849");
         Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenMobileNoInvalid() {
-        boolean result = false;
-        try {
-            result = userRegistration.validateMobileNo("2928824525");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
-        Assert.assertFalse(result);
-    }
 
     @Test
     public void givenPasswordValid() {
         boolean result = true;
-        try {
-            result = userRegistration.validatePassword("A1@12g449");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
+        result = userRegistration.validatePassword.validate("A1@12g449");
         Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenPasswordFourInvalid() {
-        boolean result = false;
-        try {
-            result = userRegistration.validatePassword("dfffds");
-        } catch (UserRegistrationException e) {
-
-            e.printStackTrace();
-        }
-        Assert.assertFalse(result);
-    }
 }
+
